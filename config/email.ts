59f280076415f141c5cee9c6560a8d57aa41aa76
ladypiapia <36269124+ladypiapia@users.noreply.github.com>
2023-12-22 14:11:35 +0800

    Update email.ts

--- config/email.ts
+++ config/email.ts
@@ -1,7 +1,7 @@
 export const emailConfig = {
-  from: 'hi@cali.so',
+  from: 'admin@oneamaze.com',
   baseUrl:
     process.env.VERCEL_ENV === 'production'
-      ? `https://cali.so`
+      ? `https://oneamaze.com`
       : 'http://localhost:3000',
 }
